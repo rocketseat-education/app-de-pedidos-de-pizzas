b0f164be7fb7d6070c22bc14d565422b7fb66503
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-26 22:28:58 -0300

    Path Mapping

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ts',
+            '.tsx',
+            '.js',
+            '.json'
+          ],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+          }
+        }
+      ]
+    ]
   };
 };
