3243ec5d287c700838ed6f65b77c90a04618cf10
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-27 08:22:41 -0300

    Utilizando do LinearGradient

--- App.tsx
+++ App.tsx
@@ -4,9 +4,9 @@ import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import { ThemeProvider } from 'styled-components/native';
 
-import theme from '@src/theme';
-
 import { SignIn } from '@screens/SignIn';
+
+import theme from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export const Container = styled.View``;
+export const Container = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT,
+  start: { x: 0, y: 1 },
+  end: { x: 0.5, y: 0.5 }
+}))`
+  flex: 1;
+  justify-content: center;
+`;
