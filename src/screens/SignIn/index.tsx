4fb74e8764f8d4349036f57bbbeed376d11ab717
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-27 15:15:10 -0300

    Componente Input

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import { Input } from '@components/Input';
+
 import { Container } from './styles';
 
 export function SignIn() {
   return (
     <Container>
+
+      <Input
+        placeholder="E-mail"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+
+      <Input
+        placeholder="Senha"
+        type="secondary"
+        secureTextEntry
+      />
 
     </Container>
   );
