6ed5622a59135558672ffd0b474756259f8bb3a4
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-27 15:45:35 -0300

    Finalizando do SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,32 +1,53 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+
+import brandImg from '@assets/brand.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordLabel
+} from './styles';
 
 export function SignIn() {
   return (
     <Container>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <Content>
 
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
+          <Brand source={brandImg} />
 
-      <Input
-        placeholder="Senha"
-        type="secondary"
-        secureTextEntry
-      />
+          <Title>Login</Title>
 
-      <Button
-        title="Entrar"
-        type="secondary"
-      />
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
 
+          <Input
+            placeholder="Senha"
+            type="secondary"
+            secureTextEntry
+          />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button
+            title="Entrar"
+            type="secondary"
+          />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,5 +1,6 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled(LinearGradient).attrs(({ theme }) => ({
   colors: theme.COLORS.GRADIENT,
@@ -8,4 +9,47 @@ export const Container = styled(LinearGradient).attrs(({ theme }) => ({
 }))`
   flex: 1;
   justify-content: center;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace() + 48
+  },
+})`
+  width: 100%;
+  padding: 0 32px;
+`;
+
+export const Title = styled.Text`
+  font-size: 32px;
+  margin-bottom: 24px;
+  align-self: flex-start;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TITLE};
+    color: ${theme.COLORS.TITLE};
+  `};
+`;
+
+export const Brand = styled.Image.attrs({
+  resizeMode: 'contain'
+})`
+  height: 340px;
+  margin-top: 64px;
+  margin-bottom: 32px;
+`;
+
+export const ForgotPasswordButton = styled.TouchableOpacity`
+  align-self: flex-end;
+  margin-bottom: 20px;
+`;
+
+export const ForgotPasswordLabel = styled.Text`
+  font-size: 14px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.TITLE};
+  `};
 `;
