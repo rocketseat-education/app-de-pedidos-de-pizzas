59cbf24aca495f093678088d4b274f94959cd7b1
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-27 17:15:27 -0300

    Buscando os dados do usuário

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -6,10 +6,18 @@ import React, {
 } from 'react';
 import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+type User = {
+  id: string;
+  name: string;
+  isAdmin: boolean;
+}
 
 type AuthContextData = {
   signIn: (email: string, password: string) => Promise<void>;
   isLogging: boolean;
+  user: User | null;
 }
 
 type AuthProviderProps = {
@@ -19,6 +27,7 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
   const [isLogging, setIsLogging] = useState(false);
 
   async function signIn(email: string, password: string) {
@@ -31,7 +40,24 @@ function AuthProvider({ children }: AuthProviderProps) {
     auth()
       .signInWithEmailAndPassword(email, password)
       .then(account => {
-        console.log(account);
+        firestore()
+          .collection('users')
+          .doc(account.user.uid)
+          .get()
+          .then(profile => {
+            const { name, isAdmin } = profile.data() as User;
+
+            if (profile.exists) {
+              const userData = {
+                id: account.user.uid,
+                name,
+                isAdmin
+              };
+
+              setUser(userData);
+            }
+          })
+          .catch(() => Alert.alert('Login', 'Não foi possível buscar os dados de perfil do usuário.'));
       })
       .catch(error => {
         const { code } = error;
