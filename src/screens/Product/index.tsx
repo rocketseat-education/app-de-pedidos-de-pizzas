776ac77e7be607d268d91995f4c95894acd0477b
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-28 10:51:04 -0300

    Button Back

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
 
+import { ButtonBack } from '@components/ButtonBack';
+
 import { Container, Header, Title, DeleteLabel } from './styles';
 
 export function Product() {
@@ -8,7 +10,10 @@ export function Product() {
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
 
       <Header>
+        <ButtonBack />
+
         <Title>Cadastrar</Title>
+
         <TouchableOpacity>
           <DeleteLabel>Deletar</DeleteLabel>
         </TouchableOpacity>
