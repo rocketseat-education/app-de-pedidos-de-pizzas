5eaac8c40667b94723b32bda47e48251964a54bb
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-28 10:43:45 -0300

    Header de produto

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, TouchableOpacity } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Header, Title, DeleteLabel } from './styles';
 
 export function Product() {
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
 
+      <Header>
+        <Title>Cadastrar</Title>
+        <TouchableOpacity>
+          <DeleteLabel>Deletar</DeleteLabel>
+        </TouchableOpacity>
+      </Header>
     </Container>
   )
 }
--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -1,6 +1,37 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.KeyboardAvoidingView`
  flex: 1;
  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT
+}))`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: ${getStatusBarHeight() + 33}px 20px 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TITLE};
+    color: ${theme.COLORS.TITLE};
+  `};
+`;
+
+export const DeleteLabel = styled.Text`
+  font-size: 14px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.TITLE};
+  `};
 `;
