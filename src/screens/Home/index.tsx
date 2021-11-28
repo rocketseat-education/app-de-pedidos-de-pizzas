fa2b1fea8fd9767a90e0c5a3412ecf32f1de89b5
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-28 15:32:00 -0300

    Header da Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components/native';
 
-import { Container } from './styles';
+import happyEmoji from '@assets/happy.png';
+
+import {
+  Container,
+  Header,
+  Greeting,
+  GreetingEmoji,
+  GreetingText
+} from './styles';
 
 export function Home() {
+  const { COLORS } = useTheme();
   return (
     <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin</GreetingText>
+        </Greeting>
 
+        <TouchableOpacity>
+          <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
     </Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,38 @@
 import styled, { css } from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT
+}))`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: ${getStatusBarHeight() + 33}px 24px 58px;
+`;
+
+export const Greeting = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const GreetingEmoji = styled.Image`
+  height: 32px;
+  width: 32px;
+  margin-right: 12px;
+`;
+
+export const GreetingText = styled.Text`
+  font-size: 20px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TITLE};
+    color: ${theme.COLORS.TITLE};
+  `};
 `;
