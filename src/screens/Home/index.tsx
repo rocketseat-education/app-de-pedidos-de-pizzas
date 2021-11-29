1614decfbe2905531ffbce9e8f29371d955f017c
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-29 12:04:04 -0300

    Buscando pizzas cadastradas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useEffect } from 'react';
+import { TouchableOpacity, Alert } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components/native';
+import firestore from '@react-native-firebase/firestore';
 
 import happyEmoji from '@assets/happy.png';
 
 import { Search } from '@components/Search';
-import { ProductCard } from '@components/ProductCard';
+import { ProductCard, ProductProps } from '@components/ProductCard';
 
 import {
   Container,
@@ -21,6 +22,35 @@ import {
 
 export function Home() {
   const { COLORS } = useTheme();
+
+  function fetchPizzas(value: string) {
+    const formattedValue = value.toLocaleLowerCase().trim();
+
+    firestore()
+      .collection('pizzas')
+      .orderBy('name_insensitive')
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then(response => {
+        const data = response.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          }
+        }) as ProductProps[];
+
+        console.log(data);
+      })
+      .catch(() => Alert.alert('Consulta', 'Não foi possível realizar a consulta'));
+  }
+
+
+  useEffect(() => {
+    fetchPizzas('');
+  }, []);
+
+
   return (
     <Container>
       <Header>
