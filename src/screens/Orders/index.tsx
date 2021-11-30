71f2a18c41f72407d90cf72ece908f7c04ee4020
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-30 20:53:39 -0300

    Entregando a pizza

--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 
 import { useAuth } from '@hooks/auth';
@@ -13,6 +13,23 @@ export function Orders() {
   const [orders, setOrders] = useState<OrderProps[]>([]);
 
   const { user } = useAuth();
+
+  function handlePizzaDelivered(id: string) {
+    Alert.alert('Pedido', 'Confirmar que a pizza foi entregue?', [
+      {
+        text: 'Não',
+        style: 'cancel'
+      },
+      {
+        text: 'Sim',
+        onPress: () => {
+          firestore().collection('orders').doc(id).update({
+            status: 'Entregue'
+          });
+        }
+      }
+    ]);
+  }
 
   useEffect(() => {
     const subscribe = firestore()
@@ -42,14 +59,18 @@ export function Orders() {
         data={orders}
         keyExtractor={item => item.id}
         renderItem={({ item, index }) => (
-          <OrderCard index={index} data={item} />
+          <OrderCard
+            index={index}
+            data={item}
+            disabled={item.status === 'Entregue'}
+            onPress={() => handlePizzaDelivered(item.id)}
+          />
         )}
         numColumns={2}
         showsVerticalScrollIndicator={false}
         ItemSeparatorComponent={() => <ItemSeparator />}
         contentContainerStyle={{ paddingHorizontal: 24, paddingBottom: 125 }}
       />
-
     </Container>
   )
 }
