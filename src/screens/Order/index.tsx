e2e5b4084cf6efb36cf6b82279c71fdf590740d7
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-29 21:21:21 -0300

    Header do pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { Container } from './styles';
+import { ButtonBack } from '@components/ButtonBack';
+
+import { Container, Header } from './styles';
 
 export function Order() {
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <Header>
+        <ButtonBack
+          onPress={() => { }}
+          style={{ marginBottom: 108 }}
+        />
 
+      </Header>
     </Container>
   )
 }
--- src/screens/Order/styles.ts
+++ src/screens/Order/styles.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.KeyboardAvoidingView`
   flex: 1;
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT
+}))`
+  padding: ${getStatusBarHeight() + 34}px 24px 0;
 `;
